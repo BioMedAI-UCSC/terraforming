77c368c11322b051c85271dd83f48026cb1bdc88
timiad24 <timiadumandan1@gmail.com>
2026-06-04 14:07:32 -0700

    Loop Dipole Moment Function Added

--- package/src/interventions/magneticfield.py
+++ package/src/interventions/magneticfield.py
@@ -4,3 +4,29 @@ Functions in this file will verify the document diff equations in task assigned.
 we want the calculations in the documents
 
 """
+
+def compute_loop_dipole_moment(
+	loop_current: float,
+	radius: float,
+) ->float:
+	"""
+Equation: M = I * pi * R^2
+
+This computes the magnetic dipole moment of a superconducting loop around Mars
+
+Important parameters:
+loop_current : float
+	current through loop (A)
+
+radius:float
+	radius of loop (m)
+
+Returns
+float
+	dipole moment (A*m^2)
+"""
+
+	loop_area = math.pi * radius**2 #computing are enclosed by loop
+	dipole_moment = loop_current * loop_area #applying the dipole moment equation
+	return dipole_moment
+
